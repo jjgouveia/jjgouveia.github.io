d60deba477bc9e8112f960992d0789d75e87939e
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-09 16:22:14 -0300

    Bug

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,60 +1,58 @@
 import React from 'react';
-import { Fade, Slide } from 'react-awesome-reveal';
+import { Slide } from 'react-awesome-reveal';
 
 export default function Skills() {
   return (
     <div className="skills">
       <h3 className="skills__tittle">Skills</h3>
-      <Slide direction="right">
-        <Fade cascade damping={0.1} triggerOnce childClassName="skills__icon">
-          <ul className="skills__container">
-            <li className="skills__icon">
-              <i className="fab fa-html5" />
-              <span>HTML</span>
-            </li>
-            <li className="skills__icon">
-              <i className="fab fa-css3-alt" />
-              <span>CSS/SCSS</span>
-            </li>
-            <li className="skills__icon">
-              <i className="fab fa-js" />
-              <span>JS/TS</span>
-            </li>
-            <li className="skills__icon">
-              <i className="fa-brands fa-node" />
-              <span>NodeJS</span>
-            </li>
-            <li className="skills__icon">
-              <i className="fa-solid fa-server" />
-              <span>Express</span>
-            </li>
-            <li className="skills__icon">
-              <i className="fa-sharp fa-solid fa-database" />
-              <span>mySQL</span>
-            </li>
-            <li className="skills__icon">
-              <i className="fa-brands fa-react" />
-              <span>React</span>
-            </li>
-            <li className="skills__icon">
-              <i className="fa-brands fa-angular" />
-              <span>Angular 10+</span>
-            </li>
-            <li className="skills__icon">
-              <i className="fa-solid fa-server" />
-              <span>Adonis JS</span>
-            </li>
-            <li className="skills__icon">
-              <i className="fa-sharp fa-solid fa-database" />
-              <span>mongoDB</span>
-            </li>
-            <li className="skills__icon">
-              <i className="fa-brands fa-git" />
-              <span>Git</span>
-            </li>
-          </ul>
-        </Fade>
-      </Slide>
+      <ul className="skills__container">
+        <Slide direction="right">
+          <li className="skills__icon">
+            <i className="fab fa-html5" />
+            <span>HTML</span>
+          </li>
+          <li className="skills__icon">
+            <i className="fab fa-css3-alt" />
+            <span>CSS/SCSS</span>
+          </li>
+          <li className="skills__icon">
+            <i className="fab fa-js" />
+            <span>JS/TS</span>
+          </li>
+          <li className="skills__icon">
+            <i className="fa-brands fa-node" />
+            <span>NodeJS</span>
+          </li>
+          <li className="skills__icon">
+            <i className="fa-solid fa-server" />
+            <span>Express</span>
+          </li>
+          <li className="skills__icon">
+            <i className="fa-sharp fa-solid fa-database" />
+            <span>mySQL</span>
+          </li>
+          <li className="skills__icon">
+            <i className="fa-brands fa-react" />
+            <span>React</span>
+          </li>
+          <li className="skills__icon">
+            <i className="fa-brands fa-angular" />
+            <span>Angular 10+</span>
+          </li>
+          <li className="skills__icon">
+            <i className="fa-solid fa-server" />
+            <span>Adonis JS</span>
+          </li>
+          <li className="skills__icon">
+            <i className="fa-sharp fa-solid fa-database" />
+            <span>mongoDB</span>
+          </li>
+          <li className="skills__icon">
+            <i className="fa-brands fa-git" />
+            <span>Git</span>
+          </li>
+        </Slide>
+      </ul>
     </div>
   );
 }
