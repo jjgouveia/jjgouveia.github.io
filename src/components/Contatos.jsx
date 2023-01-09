bcff8fcbd512810473170e5c08d99643769dbe6b
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-09 16:08:03 -0300

    Adicionada novas animações e transições

--- src/components/Contatos.jsx
+++ src/components/Contatos.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Slide } from 'react-awesome-reveal';
+import { Fade, Slide } from 'react-awesome-reveal';
 
 export default function Contatos() {
   return (
@@ -11,70 +11,76 @@ export default function Contatos() {
         <li>
           <h3>Contato</h3>
         </li>
-        <li>
-          <div className="contato-item">
-            <i className="fas fa-phone contato-item__icon" />
-            <span className="contato-iten__text">(81) 9 9173-4478</span>
-          </div>
-        </li>
-        <li>
-          <div className="contato-item contato-item__icon">
-            <i className="fas fa-envelope" />
-            <span className="contato-iten__text">gouvik.dev@gmail.com</span>
-          </div>
-        </li>
-        <li>
-          <div className="contato-item contato-item__icon">
-            <i className="fab fa-whatsapp" />
-            <span className="contato-iten__text">(81) 9 9173-4478</span>
-          </div>
-        </li>
+        <Slide>
+          <li>
+            <div className="contato-item">
+              <i className="fas fa-phone contato-item__icon" />
+              <span className="contato-iten__text">(81) 9 9173-4478</span>
+            </div>
+          </li>
+          <li>
+            <div className="contato-item contato-item__icon">
+              <i className="fas fa-envelope" />
+              <span className="contato-iten__text">gouvik.dev@gmail.com</span>
+            </div>
+          </li>
+          <li>
+            <div className="contato-item contato-item__icon">
+              <i className="fab fa-whatsapp" />
+              <span className="contato-iten__text">(81) 9 9173-4478</span>
+            </div>
+          </li>
+        </Slide>
       </ul>
       <ul className="social-container">
-        <li className="social">
-          <a
-            href="https://www.linkedin.com/in/jarbasgouveia/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-linkedin" />
-          </a>
-        </li>
-        <li className="social">
-          <a
-            href="https://github.com/jjgouveia"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-github" />
-          </a>
-        </li>
-        <li className="social">
-          <a
-            href="https://www.instagram.com/junior_gouveia"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-instagram" />
-          </a>
-        </li>
+        <Fade>
+          <li className="social">
+            <a
+              href="https://www.linkedin.com/in/jarbasgouveia/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-linkedin" />
+            </a>
+          </li>
+          <li className="social">
+            <a
+              href="https://github.com/jjgouveia"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-github" />
+            </a>
+          </li>
+          <li className="social">
+            <a
+              href="https://www.instagram.com/junior_gouveia"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-instagram" />
+            </a>
+          </li>
+        </Fade>
       </ul>
       <ul className="link-container">
         <li>
           <h3>Links</h3>
         </li>
-        <li>
-          <a href="#home">Início</a>
-        </li>
-        <li>
-          <a href="#sobre-mim">Sobre mim</a>
-        </li>
-        <li>
-          <a href="#projetos">Projetos</a>
-        </li>
-        <li>
-          <a href="#contato">Contato</a>
-        </li>
+        <Slide direction="right">
+          <li>
+            <a href="#home">Início</a>
+          </li>
+          <li>
+            <a href="#sobre-mim">Sobre mim</a>
+          </li>
+          <li>
+            <a href="#projetos">Projetos</a>
+          </li>
+          <li>
+            <a href="#contato">Contato</a>
+          </li>
+        </Slide>
       </ul>
     </section>
   );
--- src/components/SobreMim.jsx
+++ src/components/SobreMim.jsx
@@ -9,15 +9,17 @@ export default function SobreMim() {
       </Slide>
       <div className="description">
         <h3 className="description__tittle">Quem sou?</h3>
-        <p className="description__text">
-          Sempre curioso, me apaixonei ainda criança por tecnologia. Quando
-          adolescente criei meu primeiro sistema Android personalizado e
-          desde lá a programação faz parte da minha vida. Hoje sou um
-          desenvolvedor/estudante web full stack com profundo interesse no
-          universo Back-End. Em poucas palavras, crio sites e sistemas que
-          ajudam pessoas e organizações a enfrentar seus desafios de
-          negócios e atender às suas necessidades.
-        </p>
+        <Slide className="description" childClassName="description__text" direction="left">
+          <p className="description__text">
+            Sempre curioso, me apaixonei ainda criança por tecnologia. Quando
+            adolescente criei meu primeiro sistema Android personalizado e
+            desde lá a programação faz parte da minha vida. Hoje sou um
+            desenvolvedor/estudante web full stack com profundo interesse no
+            universo Back-End. Em poucas palavras, crio sites e sistemas que
+            ajudam pessoas e organizações a enfrentar seus desafios de
+            negócios e atender às suas necessidades.
+          </p>
+        </Slide>
       </div>
     </>
   );
