96d29d58c0dc491db7555098610e6dddc6ec68fc
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-04-16 17:08:28 -0300

    feat: Configurações dos carrosséis

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -1,7 +1,17 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import { Slide } from 'react-awesome-reveal';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination } from 'swiper';
 import projectList from '../assets/projectList';
 import ProjectCard from './ProjectCard';
+import '../styles/projetos.css';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+// import required modules
 
 export default function Projetos() {
   return (
@@ -10,20 +20,39 @@ export default function Projetos() {
         <h2 className="section__tittle">Projetos</h2>
       </Slide>
       <div />
-      <div className="card-container">
-        { projectList.map(({
-          id, projectName, image, description, links,
-        }) => (
-          <ProjectCard
-            key={id}
-            name={projectName}
-            thumbnail={image}
-            description={description}
-            links={links}
-          />
+      <Swiper
+        effect="coverflow"
+        grabCursor
+        centeredSlides
+        slidesPerView="auto"
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper fixed-height"
+      >
+        <div className="card-container">
+          { projectList.map(({
+            id, projectName, image, description, links,
+          }) => (
+            <SwiperSlide className="swiper-slide-testemonial">
+              <ProjectCard
+                key={id}
+                name={projectName}
+                thumbnail={image}
+                description={description}
+                links={links}
+              />
+            </SwiperSlide>
 
-        ))}
-      </div>
+          ))}
+        </div>
+      </Swiper>
     </section>
   );
 }
--- src/components/Testemonials.jsx
+++ src/components/Testemonials.jsx
@@ -1,4 +1,15 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper';
+import '../styles/testemonials.css';
 
 const data = [
   {
@@ -34,25 +45,60 @@ const data = [
 export default function Testemonials() {
   return (
     <section className="testemonials" id="testemonials">
-      <h2 className="section__tittle">Recomendações</h2>
+
+      <h2 className="section__tittle testemonial-title">Recomendações</h2>
       <span className="section__subtittle">O que meus clientes e colegas de trabalho dizem:</span>
-      <div className="testemonials-container">
-        {data.map(({
-          id, profilePic, name, job, testemonial,
-        }) => (
-          <div className="testemonial-card" key={id}>
-            <div className="testemonial-label">
-              <img className="testemonial-profile-pic" src={profilePic} alt={name} />
-              <div className="testemonial-info">
-                <h3>{name}</h3>
-                <h4>{job}</h4>
+      <div
+        className=""
+        style={{
+          paddingBottom: '50px',
+          alignItems: 'center',
+        }}
+      >
+        <Swiper
+          effect="coverflow"
+          grabCursor
+          centeredSlides
+          slidesPerView="auto"
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          {data.map(({
+            id, profilePic, name, job, testemonial,
+          }) => (
+            <SwiperSlide className="swiper-slide-testemonial">
+              <div
+                className="testemonial-card"
+                key={id + name}
+                style={{
+                  backgroundPosition: 'center',
+                  backgroundSize: 'cover',
+                  width: '300px',
+                  height: '300px',
+                }}
+              >
+                <div className="testemonial-label">
+                  <img className="testemonial-profile-pic" src={profilePic} alt={name} />
+                  <div className="testemonial-info">
+                    <h3>{name}</h3>
+                    <h4>{job}</h4>
+                  </div>
+                </div>
+                <div className="testemonial">
+                  <p className="ttt">{testemonial}</p>
+                </div>
               </div>
-            </div>
-            <div className="testemonial">
-              <p className="ttt">{testemonial}</p>
-            </div>
-          </div>
-        ))}
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
